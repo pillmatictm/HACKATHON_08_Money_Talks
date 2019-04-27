66e744a3614f08bf7294e1e14e8007b9de14507b
pillmatictm <pilinchong@pursuit.org>
2019-04-27 16:07:42 -0400

    adds recyclers

---
+++ app/src/main/java/org/pursuit/hackathon_8_money_talks/surveyRecycler/SurveyViewHolder.java
@@ -0,0 +1,12 @@
+package org.pursuit.hackathon_8_money_talks.surveyRecycler;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class SurveyViewHolder extends RecyclerView.ViewHolder {
+
+    public SurveyViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
