20d41894583566f8f0dde87fbf8b1dd1c699c289
pillmatictm <pilinchong@pursuit.org>
2019-04-27 14:11:25 -0400

    first commit

---
+++ app/src/main/java/org/pursuit/hackathon_8_money_talks/MainActivity.java
@@ -0,0 +1,13 @@
+package org.pursuit.hackathon_8_money_talks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
