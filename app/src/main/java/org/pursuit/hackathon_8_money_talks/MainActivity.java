86262f9bac4baff70503cb866fc78a2f5209c462
Linda Pokorny <lindapokrny@pursuit.org>
2019-04-27 15:59:07 -0400

    MainFrag

--- app/src/main/java/org/pursuit/hackathon_8_money_talks/MainActivity.java
+++ app/src/main/java/org/pursuit/hackathon_8_money_talks/MainActivity.java
@@ -1,13 +1,31 @@
 package org.pursuit.hackathon_8_money_talks;
 
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import org.pursuit.hackathon_8_money_talks.fragments.FragmentInterface;
+import org.pursuit.hackathon_8_money_talks.fragments.SurveyQuestionsFrag;
+
+public class MainActivity extends AppCompatActivity implements FragmentInterface {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void startQuestionaireFrag() {
+    final Fragment surveyQuestionsFragment = SurveyQuestionsFrag.newInstance();
+    inflateFragment(surveyQuestionsFragment);
+    }
+
+    private void inflateFragment(@NonNull Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
