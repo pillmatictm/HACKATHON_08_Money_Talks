305a20d4e2c0eaf0ee1dfcb8f1690e659361d63b
pillmatictm <pilinchong@pursuit.org>
2019-04-28 17:39:39 -0400

    Merge branches 'Pilin' and 'master' of https://github.com/pillmatictm/HACKATHON_08_Money_Talks into Pilin
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/org/pursuit/hackathon_8_money_talks/SplashActivity.java
+++ app/src/main/java/org/pursuit/hackathon_8_money_talks/SplashActivity.java
@@ -1,5 +1,7 @@
 package org.pursuit.hackathon_8_money_talks;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,16 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,
+                        MainActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 4000);
     }
+
 }
